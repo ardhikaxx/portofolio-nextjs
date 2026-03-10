d2f4d25878549e401f2b64908b0629b5938b5b4f
ardhikaxx <ardhikayanuar58@gmail.com>
2026-03-11 00:35:54 +0700

    fix(project) ubah background halaman project jadi hitam

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -3,7 +3,7 @@ import NavBottom from "../components/NavBottom";
 
 export default function ProjectPage() {
     return (
-        <main className="min-h-screen bg-linear-to-b from-green-900 to-blue-900 pb-24">
+        <main className="min-h-screen bg-black">
             <Project />
             <NavBottom currentPath="/project" />
         </main>
